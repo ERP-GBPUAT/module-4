11a413f338f122dc08e2ae7f6ff1ca6f18cc2bbd
Ashutosh Kumar Singh <ashutosh44ks@gmail.com>
2023-06-01 04:06:46 +0530

    fix crash

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,15 +7,20 @@ const Home = () => {
     let data = JSON.parse(localStorage.getItem("data"));
     let token = localStorage.getItem("token");
     // if no data in localstorage, return to login page
-    if (!data || !token) navigate("/login");
-
+    if (
+      data === null ||
+      token === null ||
+      data === undefined ||
+      token === undefined
+    )
+      window.location.href = "../login";
     const isStudent = data.user.isStudent || false;
     const isFaculty = data.user.isFaculty || false;
-    const isHod =
-      data.faculty.hodOfDepartment || data.faculty.deanOfCollege || false;
-    if (isHod) navigate("/hodPortal");
-    else if (isFaculty) navigate("/advisor");
-    else if (isStudent) navigate("/advisee");
+    if (isFaculty) {
+      if (!!data.faculty.hodOfDepartment || !!data.faculty.deanOfCollege)
+        navigate("/hodPortal");
+      else navigate("/advisor");
+    } else if (isStudent) navigate("/advisee");
     else navigate("/login");
   }, []);
 
